d6420fb1a161edf7c17f509c57ee32c98f050258
MouadhBNB7741 <129070196+MouadhBNB7741@users.noreply.github.com>
2025-04-28 19:03:35 +0100

    all

--- univ-project/src/App.tsx
+++ univ-project/src/App.tsx
@@ -1,21 +1,14 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import React from 'react';
-import AppRoutes from './routes/routes';
-import { AuthProvider } from './context/AuthProvider';
-
-
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/routes";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        
-          <AppRoutes />
-         
+        <AppRoutes />
       </Router>
     </AuthProvider>
-    //
- 
   );
 }
 
